49df9776012f07a7eb8b154820ceb4bda9a03a80
eldar23451 <liyev8024@gmail.com>
2024-05-16 21:32:16 +0400

    task file

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("1.show name");
+//Console.WriteLine("2.show surname");
+//Console.WriteLine("3.show age");
+//int choice=int.Parse(Console.ReadLine());
+//switch (choice)
+//{
+//	case 1:
+//		Console.WriteLine("please enter your name");
+//		string name=Console.ReadLine();
+//		Console.WriteLine($"welcome{name}.Welcome wibsite");
+//		break;
+//	case 2:
+//		Console.WriteLine("please enter your surname");
+//		string surname=Console.ReadLine();
+//		Console.WriteLine($"welcome{surname}.welcome foundation");
+//		break ;
+//		case 3:
+//		Console.WriteLine("please enter your born year");
+//		int age=int.Parse(Console.ReadLine());
+//		int year=int.Parse(Console.ReadLine());
+//		Console.WriteLine($"you are{year}years old");
+//		break;
+
+//string name = Console.ReadLine();
+//Console.WriteLine("xos gelmisiniz" + name);
+
+//int a=int.Parse(Console.ReadLine());
+//int yas = 2024 - a;
+//Console.WriteLine(yas);
+
+
+
+
